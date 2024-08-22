b93f39d56bf58f480d6d4af4030377bc0179fb60
web-mahadihasan <mehedihasanmilu7@gmail.com>
2024-08-22 19:31:53 +0600

    I have added Navbar and Banner section using tailwidn and daisyUI

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      "primary-color": "#ABEF5F",
+      "primary-color-light": "rgba(171, 239, 95, 0.5)",
+      "text-light": "rgb(115, 115, 115)",
+      "text-dark": "rgb(73, 73, 73)",
+      "text-black": "rgb(33, 33, 33)",
+      "bg-secondary": "rgb(244, 244, 244)",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
